dd5fc083a807e0b43aec282495341bcf6560a4bd
sujalshah593 <sujalshah593@gmail.com>
2025-01-25 00:10:03 +0530

    Custom api response and error handling, util code discussion

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path: "./.env"
 })
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err) => {
+    console.log('MONGODB CONNECTION FAILED:', err);
+})
 
 // import express from "express";
 
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,18 @@
+const asynchandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export {asynchandler}
+
+// const asynchandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 404).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
